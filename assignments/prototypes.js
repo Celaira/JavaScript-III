e6bd19e8f20e225652f36528191e4c4a919dfde5
Megan <megan.miller@smoldev.dev>
2019-05-08 14:52:33 -0500

    Secondary commit
    Finished project

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,34 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game`;
+};
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(charAttributes) {
+  GameObject.call(this, charAttributes);
+  this.healthPoints = charAttributes.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+}
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,15 +55,41 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(humanAttributes) {
+  CharacterStats.call(this, humanAttributes);
+  this.team = humanAttributes.team;
+  this.weapons = humanAttributes.weapons;
+  this.language = humanAttributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`;
+}
+
+function Hero(heroAttributes) {
+  Humanoid.call(this, humanAttributes);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+
+function Villian(villianAttributes) {
+  Humanoid.call(this, villianAttributes);
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +150,10 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The Window/Console.
+* 2. A function called with dot notation attached to an object.
+* 3. Utilizing an object constructor and then the `new` keyword.
+* 4. The `.call()` and `.apply()` methods.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,40 @@
 // Principle 1
 
 // code example for Window Binding
+function myFunction(fun) {
+    console.log(this);
+    return fun;
+}
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    greet: 'Hai',
+    sayHello: function(name) {
+      console.log(`${this.greet} my name is ${name}`);
+    }
+  };
+  
+  myObj.sayHello('Megan');
 
 // Principle 3
 
 // code example for New Binding
+function myObject(person) {
+    this.name = person;
+    this.speak = function () {
+        console.log(`Hello! My name is ${this.name}.`)
+    }
+}
+
+const me = new myObject("Megan");
+const Gerard = new myObject("Gerard");
+const Leilani = new myObject("Leilani");
+me.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+me.speak.call(Gerard);
+me.speak.apply(Leilani);
